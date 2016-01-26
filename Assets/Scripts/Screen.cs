644d67718f249e9abf48b83049c37e56adaf54c1
MagicFaker <985078803@qq.com>
2016-01-26 20:12:12 +0800

    first_commit

---
+++ Assets/Scripts/Screen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screen : MonoBehaviour {
+	public Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+		//Screen.SetResolution(1280, 800, true, 60);
+		mainCamera = Camera.main;
+		//  float screenAspect = 1280 / 720;  现在android手机的主流分辨。
+		//  mainCamera.aspect --->  摄像机的长宽比（宽度除以高度）
+		mainCamera.aspect = 1.78f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
